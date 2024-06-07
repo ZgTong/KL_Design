6ab5d24b94416d9cabc21c1ca477a219abc6cb05
Zuguang Tong <tongzuguang@gmail.com>
2024-06-07 12:38:11 +1000

    debug

--- packages/frontend/src/components/widgets/ClientHosting.tsx
+++ packages/frontend/src/components/widgets/ClientHosting.tsx
@@ -4,10 +4,14 @@ import { Box, Fade } from '@mui/material';
 import Loading from '@app/loading';
 const ClientOnly: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [isLoaded, setIsLoaded] = useState(false);
-    const handleLoad = () => setIsLoaded(true);
     useEffect(() => {
-        window.addEventListener('load', handleLoad);
-        return () => window.removeEventListener('load', handleLoad);
+        const handleLoad = () => setIsLoaded(true);
+        if (document.readyState === 'complete') {
+            handleLoad();
+        } else {
+            window.addEventListener('load', handleLoad);
+            return () => window.removeEventListener('load', handleLoad);
+        }
     }, []);
     return isLoaded ? (
         <Fade in={isLoaded} timeout={1500} key={'topFade'}>
